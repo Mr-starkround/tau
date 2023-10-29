29e56f003da6450fad113ffe1d6fd686ef097ba9
ZoidsRobot <113953968+ZoidsRobot@users.noreply.github.com>
2023-10-29 20:39:32 +0800

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ from logging.handlers import RotatingFileHandler
 load_dotenv("config.env")
 
 # Bot token dari @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6813840351:AAHOJx8oztAllOVlP9879aCUiB6Tc6Xry-g")
 
 # API ID Anda dari my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "9774346"))
